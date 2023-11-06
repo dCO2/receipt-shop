f799c9c9402c7f5259697d974b239fb73d6727f4
Caleb Durojaiye <dco2.caleb@gmail.com>
2023-11-06 16:21:43 +0100

    using clerk

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,12 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div class="grid grid-rows-3 grid-flow-col gap-4">
+      <SignIn class="row-start-1 row-end-4"/>
+      <div class="row-start-1 row-end-4">
+        Hello
+      </div>
+    </div>
+  );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,13 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div class="grid grid-rows-3 grid-flow-col gap-4">
+      <SignUp class="row-start-1 row-end-4"/>
+      <div class="row-start-1 row-end-4">
+        Hello
+      </div>
+    </div>
+  );
 }
+

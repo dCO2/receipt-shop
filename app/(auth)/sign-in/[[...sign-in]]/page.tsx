7129801e4aa5d91d7221d1db731bc99bbc3dfa79
Caleb Durojaiye <dco2.caleb@gmail.com>
2023-11-07 08:54:07 +0100

    use custom sign-in

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,10 +2,13 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div class="grid grid-rows-3 grid-flow-col gap-4">
-      <SignIn class="row-start-1 row-end-4"/>
-      <div class="row-start-1 row-end-4">
-        Hello
+    <div className="grid grid-rows-3 grid-flow-col gap-4">
+      <SignIn className="row-start-1 row-end-4"/>
+      <div className="row-start-1 row-end-4">
+        This is where receipt-shop should appear. This is where receipt-shop should appear. 
+        This is where receipt-shop should appear. This is where receipt-shop should appear. 
+        This is where receipt-shop should appear. This is where receipt-shop should appear. 
+        This is where receipt-shop should appear. This is where receipt-shop should appear. 
       </div>
     </div>
   );
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,11 @@
-import { SignUp } from "@clerk/nextjs";
+// import { SignUp } from "@clerk/nextjs";
+import RegisterPage from "../../../register.jsx"
 
 export default function Page() {
   return (
-    <div class="grid grid-rows-3 grid-flow-col gap-4">
-      <SignUp class="row-start-1 row-end-4"/>
-      <div class="row-start-1 row-end-4">
+    <div className="grid grid-rows-3 grid-flow-col gap-4">
+      <RegisterPage className="row-start-1 row-end-4"/>
+      <div className="row-start-1 row-end-4">
         Hello
       </div>
     </div>

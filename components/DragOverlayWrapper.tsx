3ffe1452000c3ab227210f5d80ae4c0a6eec8990
Caleb Durojaiye <dco2.caleb@gmail.com>
2023-11-15 18:09:02 +0100

    dragoverlay now renders

--- components/DragOverlayWrapper.tsx
+++ components/DragOverlayWrapper.tsx
@@ -1,13 +1,34 @@
-import { DragOverlay, useDndMonitor } from '@dnd-kit/core'
-import React from 'react'
+import { Active, DragOverlay, useDndMonitor } from '@dnd-kit/core'
+import React, { useState } from 'react'
+import { ElementType, FactoryElements } from './FactoryElements';
+import { SidebarBtnElementDragOverlay } from './SidebarBtnElement';
 
 export default function DragOverlayWrapper() {
+  const [draggedItem, setDraggedItem] = useState<Active | null>(null);
+
   useDndMonitor({
     onDragStart: (event) => {
-      console.log("DRAG ITEM", event);
+      setDraggedItem(event.active);
+    },
+    onDragCancel: () => {
+      setDraggedItem(null);
+    },
+    onDragEnd: () => {
+      setDraggedItem(null);
     },
   });
-  const node = <div>"Node is dragged"</div>
+
+  if(!draggedItem) return null;
+
+  let node = <div></div>
+
+  const isEditorBtnElement = draggedItem.data?.current?.isEditorBtnElement;
+
+  if(isEditorBtnElement){
+    const type = draggedItem.data?.current?.type as ElementType;
+    node = <SidebarBtnElementDragOverlay factoryElement={FactoryElements[type]} />
+  }
+
   return (
     <DragOverlay>{node}</DragOverlay>
   )

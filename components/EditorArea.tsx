f4eff56cac6d263f6e9f53d6c121991fa30edf08
Caleb Durojaiye <dco2.caleb@gmail.com>
2023-11-16 22:10:29 +0100

    made editor-area view respond to dropover

--- components/EditorArea.tsx
+++ components/EditorArea.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import EditorAreaSidebar from './EditorAreaSidebar'
 import { useDroppable } from '@dnd-kit/core'
+import { cn } from '@/lib/utils';
 
 function EditorArea() {
   const droppable = useDroppable({
@@ -12,10 +13,23 @@ function EditorArea() {
   });
 
   return (
-    <div>
-      <div>
-        <div>
-          <p>Drop Here. This is your receipt.</p>
+    <div className="flex w-full h-full">
+      <div className="w-full">
+        <div 
+          ref={droppable.setNodeRef}
+          className={cn(
+            "max-w-[920px] h-full flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto",
+            droppable.isOver && "ring-2 ring-primary/20")}
+        >
+          {!droppable.isOver &&
+            <p className="flex flex-grow items-center">Drop Here. This is your receipt.</p>
+          }
+          {droppable.isOver &&
+            <div className="p-4 w-full">
+              <div className="h-[120px] bg-slate-200">
+              </div>
+            </div>
+          }
         </div>
       </div>
       <EditorAreaSidebar/>

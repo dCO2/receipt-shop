c8be0953d664c097b4c8a369791fd53a9531dde1
Caleb Durojaiye <dco2.caleb@gmail.com>
2023-11-22 16:41:38 +0100

    add comments to EditorContext code

--- components/context/EditorContext.tsx
+++ components/context/EditorContext.tsx
@@ -1,18 +1,23 @@
 "use client";
-
 import { ReactNode, createContext, useState } from "react";
 import { FactoryElementInstance } from "../FactoryElements";
 
+// type for EditorArea's context data;
+// EditorArea should have a list of factory elements;
+// EditorArea should be able to add & remove elements.
 type EditorContextType = {
   elements: FactoryElementInstance[];
   addElement: (index: number, element: FactoryElementInstance) => void;
+  removeElement: (id: string) => void;
 }
 
 export const EditorContext = createContext<EditorContextType | null>(null);
 
 export default function EditorContextProvider({children} : {children: ReactNode;}){
+  // EditorArea's context data
   const [elements, setElements] = useState<FactoryElementInstance[]>([]);
 
+  // EditorArea's context actions
   const addElement = (index: number, element: FactoryElementInstance) => {
     setElements((prev) => {
       const newElements = [...prev];
@@ -21,11 +26,14 @@ export default function EditorContextProvider({children} : {children: ReactNode;
     });
   };
 
+  const removeElement = () => {};
+
   return(
     <EditorContext.Provider
       value={{
         elements,
         addElement,
+        removeElement,
       }}
     >
       {children}
